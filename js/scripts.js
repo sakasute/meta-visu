1c58aea33b646b9de7d7cb061e28cbaab5773c30
Sakari Sutela <sakari.sutela@gmail.com>
2018-06-29 14:06:06 +0300

    remove old functions and chop updateNodes into smaller pieces

--- js/scripts.js
+++ js/scripts.js
@@ -41,8 +41,14 @@ class TreeChart {
     nodeEnter
       .append('circle')
       .attr('class', 'node__circle')
-      .attr('r', this.config.nodeSize)
-      .style('fill', 'gray');
+      .attr('r', this.config.nodeSize);
+  }
+
+  moveNodesInPlace(nodeEnter) {
+    nodeEnter
+      .transition()
+      .duration(this.config.animationDuration)
+      .attr('transform', d => `translate(${d.y}, ${d.x})`);
   }
 
   updateNodes() {
@@ -62,11 +68,7 @@ class TreeChart {
       });
 
     this.drawNodeCircles(nodeEnter);
-
-    nodeEnter
-      .transition()
-      .duration(this.config.animationDuration)
-      .attr('transform', d => `translate(${d.y}, ${d.x})`);
+    this.moveNodesInPlace(nodeEnter);
 
     // exit
     nodeSelection
@@ -135,137 +137,6 @@ async function getData(file) {
     .then(dataJson => dataJson);
 }
 
-function findChildArr(object) {
-  const childArrNames = ['registerAdmins', 'registers', 'samplings'];
-  const childrenName = childArrNames.filter(name => object[name] !== undefined)[0];
-  if (childrenName !== undefined) {
-    return object[childrenName];
-  }
-  return null;
-}
-
-function diagonal(s, d) {
-  const path = `M ${s.y} ${s.x}
-                C ${(s.y + d.y) / 2} ${s.x},
-                  ${(s.y + d.y) / 2} ${d.x},
-                  ${d.y} ${d.x}`;
-
-  return path;
-}
-
-function initializeTree(data) {
-  const margin = {
-    top: 20,
-    right: 90,
-    bottom: 20,
-    left: 90,
-  };
-  const width = 960 - margin.right - margin.left;
-  const height = 600 - margin.top - margin.bottom;
-
-  const treeSVG = d3
-    .select('body')
-    .append('svg')
-    .attr('width', width + margin.right + margin.left)
-    .attr('height', height + margin.top + margin.bottom)
-    .append('g')
-    .attr('transform', `translate(${margin.left}, ${margin.top})`);
-
-  const treeLayout = d3.tree().size([height, width]);
-
-  const hierarchy = d3.hierarchy(data, d => findChildArr(d));
-
-  const sourcePoint = {
-    x0: height / 2,
-    y0: 0,
-  };
-
-  const treeData = treeLayout(hierarchy);
-
-  return { treeData, treeSVG, sourcePoint };
-}
-
-function updateNodes(treeData, treeSVG, sourcePoint) {
-  const nodesData = treeData.descendants();
-  const nodeSelection = treeSVG
-    .selectAll('g.node')
-    .data(nodesData, d => (d.parent ? d.parent.data.name + d.data.name : d.data.name));
-
-  const nodeEnter = nodeSelection
-    .enter()
-    .append('g')
-    .attr('class', 'node')
-    .attr('transform', () => `translate(${sourcePoint.y0}, ${sourcePoint.x0})`)
-    .on('click', (d) => {
-      if (d.children) {
-        d.childrenStored = d.children;
-        d.children = null;
-      } else {
-        d.children = d.childrenStored;
-        d.childrenStored = null;
-      }
-      // NOTE: Changing d implicitly changes nodesData which implicitly changes treeData.
-      updateNodes(treeData, treeSVG, {
-        x0: d.x,
-        y0: d.y,
-      });
-      updateLinks(treeData, treeSVG, {
-        x0: d.x,
-        y0: d.y,
-      });
-    });
-
-  nodeEnter
-    .append('circle')
-    .attr('class', 'node__circle')
-    .attr('r', 10)
-    .style('fill', 'gray');
-
-  nodeEnter
-    .transition()
-    .duration(750)
-    .attr('transform', d => `translate(${d.y}, ${d.x})`);
-
-  const nodeExit = nodeSelection
-    .exit()
-    .transition()
-    .duration(750)
-    .attr('transform', `translate(${sourcePoint.y0}, ${sourcePoint.x0})`)
-    .remove();
-}
-
-function updateLinks(treeData, treeSVG, sourcePoint) {
-  const linksData = treeData.descendants().slice(1);
-  const linkSelection = treeSVG
-    .selectAll('path.link')
-    .data(linksData, d => (d.parent ? d.parent.data.name + d.data.name : d.data.name));
-
-  // enter
-  const linkEnter = linkSelection
-    .enter()
-    .insert('path', 'g')
-    .attr('class', 'link')
-    .attr('d', () => {
-      const o = { x: sourcePoint.x0, y: sourcePoint.y0 };
-      return diagonal(o, o);
-    });
-
-  linkEnter
-    .transition()
-    .duration(750)
-    .attr('d', d => diagonal(d, d.parent));
-
-  const linkExit = linkSelection
-    .exit()
-    .transition()
-    .duration(750)
-    .attr('d', () => {
-      const o = { y: sourcePoint.y0, x: sourcePoint.x0 };
-      return diagonal(o, o);
-    })
-    .remove();
-}
-
 async function main() {
   const data = await getData('poiminnat.json');
 
@@ -281,7 +152,7 @@ async function main() {
       height: 600,
     },
     animationDuration: 750,
-    nodeSize: 10,
+    nodeSize: 7.5,
   };
 
   const treeSVG = d3.select('body').append('svg');
