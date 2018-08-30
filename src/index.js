effd1ef18770beb6d6ebec17e5a722ad9dd6e0a3
Sakari Sutela <sakari.sutela@gmail.com>
2018-08-30 15:50:46 +0300

    move manual polyfills into own file

--- src/index.js
+++ src/index.js
@@ -1,37 +1,6 @@
 import TreeChart from './TreeChart';
 import CategoryTimeline from './CategoryTimeline';
 
-// HACK: "manual" polyfills
-/* eslint-disable */
-if (window.NodeList && !NodeList.prototype.forEach) {
-  NodeList.prototype.forEach = function(callback, thisArg) {
-    thisArg = thisArg || window;
-    for (let i = 0; i < this.length; i++) {
-      callback.call(thisArg, this[i], i, this);
-    }
-  };
-}
-
-if (window.NodeList && !NodeList.prototype.remove) {
-  // from:https://github.com/jserz/js_piece/blob/master/DOM/ChildNode/remove()/remove().md
-  (function nodelistRemovePolyfill(arr) {
-    arr.forEach(item => {
-      if (item.hasOwnProperty('remove')) {
-        return;
-      }
-      Object.defineProperty(item, 'remove', {
-        configurable: true,
-        enumerable: true,
-        writable: true,
-        value: function remove() {
-          if (this.parentNode !== null) this.parentNode.removeChild(this);
-        },
-      });
-    });
-  })([Element.prototype, CharacterData.prototype, DocumentType.prototype]);
-}
-/* eslint-enable */
-// ********
 function compareByName(a, b) {
   if (a.name < b.name) {
     return -1;
@@ -237,7 +206,7 @@ async function main() {
   const toggleBtn = document.querySelector('.js-toggle-menu');
   toggleBtn.addEventListener('click', () => {
     document.querySelector('.js-nav-card').classList.toggle('nav--closed');
-    toggleBtn.classList.toggle('nav__toggle-btn--open');
+    toggleBtn.classList.toggle('nav__toggle-btn--rotate');
   });
 
   document
