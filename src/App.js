dbc168abb001eae1f58859b1bbc586d1f3ead894
Sakari Sutela <sakari.sutela@gmail.com>
2018-09-06 16:27:02 +0300

    basic componentDidMount case

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filenames: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch('data/filenames.json')
+      .then(res => res.json())
+      .then(filenames => filenames.sort())
+      .then(filenames => this.setState({ filenames }));
+  }
+
   render() {
+    const { filenames } = this.state;
+    const filenameLis = filenames.map(filename => <li key>{filename}</li>);
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <ul>{filenameLis}</ul>
+        <div className="sidebar-placeholder" />
+        <main className="chart-area" />
       </div>
     );
   }
